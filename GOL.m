d44ffaf13f7325575ee81d510fe92aa6b366b330
felgus <felix@pixotag.com>
2018-12-20 11:04:02 +0100

    Gjorde typ en hard reset, koden funkar lite bättre nu tho

---
+++ GOL.m
@@ -0,0 +1,21 @@
+function s=GOL (A,B,i,j)
+% game of life rule
+sm=0;
+% count number of alive neighbors
+sm=sm+ B(i-1,j-1)+B(i-1,j)+B(i-1,j+1);
+sm=sm+ B(i,j-1)+           B(i,j+1);
+sm=sm+ B(i+1,j-1)+B(i+1,j)+B(i+1,j+1);
+
+% compute the new state of the current cell
+s=B(i,j);
+if B(i,j)==1
+    if (sm>2)&&(sm<5)
+        s=1;
+    else
+        s=0 ;   
+    end
+else
+    if sm==3
+       s=1;
+    end
+end
